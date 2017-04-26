167f08cffd22c1f88c0db42cfdd458091556c5e1
joel-matthews <joel.matthews94@gmail.com>
2017-04-26 16:35:25 -0400

    Updated connection to database

--- insert.php
+++ insert.php
@@ -2,13 +2,13 @@
 // if(isset($_POST['submit']))
 require('database.php');
 {
-	$fname=$_POST['fname'];
-	$lname=$_POST['lname'];
-	$email=$_POST['email'];
-	$phone=$_POST['phone'];
-	$birthday=$_POST['birthday'];
-	$gender=$_POST['gender'];
-	$password=$_POST['password'];
+	$fName=$_POST['fname'];
+	$lName=$_POST['lname'];
+	$Email=$_POST['Email'];
+	$Phone=$_POST['Phone'];
+	$DOB=$_POST['DOB'];
+	$Gender=$_POST['Gender'];
+	$Password=$_POST['Password'];
 
 	$sql="INSERT INTO Accounts (fName, lName, Email, Phone, DOB, Gender, Password) VALUES ('$fName','$lName','$Email','$Phone','$DOB','$Gender','$Password')";
 
